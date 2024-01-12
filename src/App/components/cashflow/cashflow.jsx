d0933882962c8490e61683a92bdbe93840e4ef5b
Rick Lochner <ricklochner@MacBook-Air.local>
2024-01-12 08:07:01 +0100

    adding balance and assets in other parts

--- src/App/components/cashflow/cashflow.jsx
+++ src/App/components/cashflow/cashflow.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Monatsauswahl from './monatsauswahl'
+import CashflowBalance from './cashflowBalance'
 
 
 export default function Cashflow() {
   return (
+    <div>
     <div className='cashflow-header'>
       <div className="cashflow-title"><h1>Dein Cashflow</h1></div>
       <Monatsauswahl />
       <div></div>
+    </div>
+      <div className="cashflow-oben">
+        <div className="cashflow-income"></div>
+      <CashflowBalance />
+    </div>
+      <div className="cashflow-outcome">
+        <div>Hier ausgaben</div>
+      </div>
     </div>
   )
 }
---
+++ src/App/components/cashflow/cashflowBalance.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import "../../../app.css"
+
+export default function CashflowBalance() {
+    const auth = sessionStorage.getItem("userid")
+    const [balance, setBalance] = useState()
+
+
+    const config = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    useEffect(()=>{
+        fetchData();
+    }, []); 
+
+    const fetchData = async () => {
+        try{
+            const response = await fetch(`https://dowstack.onrender.com/users/${auth}`, config);
+            const data = await response.json(); 
+            console.log("data", data)
+            setBalance(data.balance)
+            console.log("balance", data.balance)
+        }catch(err) {
+        console.log("irgendwas stimmt nicht", err);
+    }
+    }; 
+  return (
+    <div className='dashboard-balance app-boxshadow'>
+      <h2>Du hast diesen Monat noch {balance}€</h2>
+    </div>
+  )
+}
