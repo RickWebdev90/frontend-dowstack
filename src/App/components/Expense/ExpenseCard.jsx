3b3cf9737f1bc3a5de399e95c404be2c29bf1061
Wowa <rimidalf@tutamail.com>
2024-01-15 17:49:57 +0100

    Display Date on Cashflow & raw Delete -> without HTTP operation yet

--- src/App/components/Expense/ExpenseCard.jsx
+++ src/App/components/Expense/ExpenseCard.jsx
@@ -1,12 +1,27 @@
-const ExpenseCard = ({ title, amount, recurring, date }) => {
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+
+const ExpenseCard = ({ _id, title, amount, recurring, date }) => {
+  const rawDate = date;
+  const parsedDate = new Date(rawDate);
+  const formatDate = format(parsedDate, "dd.MM.yyyy");
+  const [entryId, setEntryId] = useState("");
+  useEffect(() => {
+    setEntryId(_id);
+  }, []);
   return (
     <>
       <div className="ExpenseEntry">
         <div className="ExpenseTitle">{title}</div>
         <div className="ExpenseAmount">{`${amount} €`}</div>
         <div className="ExpenseStatus">{recurring ? "🔁" : "1️⃣"}</div>
-        <div className="ExpenseDate">{date}</div>
-        <button className="ExpenseButton">Bearbeiten</button>
+        <div className="ExpenseDate">{formatDate}</div>
+        <button
+          className="ExpenseButton"
+          onClick={() => console.log("Delete Entry with ID: ", entryId)}
+        >
+          Löschen
+        </button>
       </div>
     </>
   );
